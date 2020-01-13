65eae17e06a65a0bbcea97b2493df7282c168fea
Junya.Satou <jj.ruri11280506@gmail.com>
2020-01-13 16:40:27 +0900

    コメントアウト追加

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -8,14 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
+//コメントアウト
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
 
