5cde4128ed191b8f0598fe5ee5f119bdb58203fb
Junya.Satou <jj.ruri11280506@gmail.com>
2020-01-14 11:08:20 +0900

    segueと新しい画面を追加

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by 佐藤淳哉 on 2020/01/14.
+//  Copyright © 2020 Junya.Satou. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
