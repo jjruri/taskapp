b7a42197968b87b88738b1e43a556406cec86f55
Junya.Satou <jj.ruri11280506@gmail.com>
2020-01-19 23:08:34 +0900

    RealmSwiftのインポートと、viewcontrollerへの記載

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,9 +7,20 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    // Realmインスタンスを取得する
+    let realm = try! Realm()
+    
+    // DB内のタスクが格納されるリスト。
+    // 日付の近い順でソート：昇順
+    // 以降内容をアップデートするとリスト内は自動的に更新される。
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
