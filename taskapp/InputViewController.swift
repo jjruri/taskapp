2305f815646698873a83f8a77186dfe7b67eedde
Junya.Satou <jj.ruri11280506@gmail.com>
2020-01-19 22:55:44 +0900

    podfileとgitignoreの作成まで完了

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class InputViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
